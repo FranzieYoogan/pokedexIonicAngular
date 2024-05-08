7f61a9f46229550d2dbe036c3b9bf13d3e9f60c6
FrankieSilvaSantos <frankiesilvasantos@gmail.com>
2024-05-08 13:39:29 -0300

    animationsLogin

--- something/src/app/tab1/tab1.page.ts
+++ something/src/app/tab1/tab1.page.ts
@@ -1,4 +1,4 @@
-import { Component, inject } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
 import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
 import { ExploreContainerComponent } from '../explore-container/explore-container.component';
 import { HeaderPage } from '../header/header.page';
@@ -11,12 +11,87 @@ import { HttpClient, HttpClientModule } from '@angular/common/http';
   standalone: true,
   imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent, HeaderPage, HttpClientModule],
 })
-export class Tab1Page {
+export class Tab1Page implements OnInit {
   constructor() {}
 
   private http = inject(HttpClient)
 
     data: any
+
+
+  ngOnInit(): void {
+    
+    const email:any = document.getElementById('email')
+    const password: any = document.getElementById('password')
+    const running:any = document.getElementById('running')
+    const running2:any = document.getElementById('running2')
+
+
+    email.addEventListener('input', (event: { target: any; }) => {
+
+      if(event.target.value != "") {
+        
+          running.style.transform = "scaleX(-1)"
+
+          setTimeout(() => {
+
+            running.style.visibility = "visible"
+            running.style.transition = "1s"
+            running.style.left = "80%"
+            
+          }, 300);
+        
+          
+        
+      } else {
+        running.style.transform = "scaleX(1)"
+        setTimeout(() => {
+          running.style.transition = "1s"
+          running.style.left = "3%"
+        }, 500);
+  
+        setTimeout(() => {
+          running.style.visibility = "hidden"
+        }, 700);
+       
+
+      }
+
+
+    })
+
+    password.addEventListener('input', (event2: { target: any; }) => {
+
+      if(event2.target.value != "") {
+        running2.style.transform = "scaleX(-1)"
+
+        setTimeout(() => {
+          running2.style.visibility = "visible"
+          running2.style.transition = "1s"
+          running2.style.left = "80%"
+        }, 300);
+     
+        
+      } else {
+
+        running2.style.transform = "scaleX(1)"
+        setTimeout(() => {
+          running2.style.transition = "1s"
+          running2.style.left = "3%"
+        }, 500);
+  
+        setTimeout(() => {
+          running2.style.visibility = "hidden"
+        }, 700);
+
+      }
+
+
+    })
+
+  }
+
+
   login() {
 
     const email:any = document.getElementById('email')
