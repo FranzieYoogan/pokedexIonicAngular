f8c1707cef66f0244dad0337af22745afee00fca
Franzie Yoogan <franzieyoogan@gmail.com>
2024-05-24 18:53:22 -0300

    animations

--- something/src/app/signup2/signup2.page.ts
+++ something/src/app/signup2/signup2.page.ts
@@ -16,11 +16,82 @@ export class Signup2Page implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    const email:any = document.getElementById('email2')
+    const password: any = document.getElementById('password2')
+    const running:any = document.getElementById('running')
+    const running2:any = document.getElementById('running2')
+   
+
+
+    email.addEventListener('input', (event: { target: any; }) => {
+
+      if(event.target.value != "") {
+        
+          running.style.transform = "scaleX(-1)"
+
+          setTimeout(() => {
+
+            running.style.visibility = "visible"
+            running.style.transition = "1s"
+            running.style.left = "80%"
+            
+          }, 300);
+        
+          
+        
+      } else {
+        running.style.transform = "scaleX(1)"
+        setTimeout(() => {
+          running.style.transition = "1s"
+          running.style.left = "3%"
+        }, 500);
+  
+        setTimeout(() => {
+          running.style.visibility = "hidden"
+        }, 700);
+       
+
+      }
+
+
+    })
+
+    password.addEventListener('input', (event2: { target: any; }) => {
+
+      if(event2.target.value != "") {
+        running2.style.transform = "scaleX(-1)"
+
+        setTimeout(() => {
+          running2.style.visibility = "visible"
+          running2.style.transition = "1s"
+          running2.style.left = "80%"
+        }, 300);
+     
+        
+      } else {
+
+        running2.style.transform = "scaleX(1)"
+        setTimeout(() => {
+          running2.style.transition = "1s"
+          running2.style.left = "3%"
+        }, 500);
+  
+        setTimeout(() => {
+          running2.style.visibility = "hidden"
+        }, 700);
+
+      }
+
+
+    })
+
+
   }
+
 
   signUp() {
 
-    
+
   }
 
 }
